4121b0f96ca754646f97a02eb1d6ccc8b60a5846
YouMingYeh <b10705052@ntu.edu.tw>
2024-02-16 06:21:04 +0800

    fix: improve commit message prompt

--- utils/ai.js
+++ utils/ai.js
@@ -18,33 +18,39 @@ export async function checkGeminiEnv() {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function runAI(stagedChanges) {
-  async function run(stagedChanges) {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    async function run(stagedChanges) {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const request = {
-      contents: [
-        {
-          role: "user",
-          parts: [
-            {
-              text:
-                "Given the following `git diff --cached` output, generate a concise, relevant commit message. Focus solely on summarizing the changes. \nRules: \n1. The commit message must be concise and not exceed 50 characters. \n2. Focus on summarizing the changes without providing explanations or reasons. \n3. Do not include any imaginative or unrelated content. 4. If the changes are small, no verbose. \nContext (git diff --cached output): " +
-                stagedChanges,
-            },
-          ],
-        },
-      ],
-    };
+        const request = {
+            contents: [
+                {
+                    role: "user",
+                    parts: [
+                        {
+                            text: `Given the following 'git diff --cached' output, generate a concise, relevant commit message.
+Rules:
+1. The commit message must be concise and not exceed 50 characters.
+2. Focus on summarizing the changes without providing explanations or reasons.
+3. Do not include any imaginative or unrelated content.
+4. If the changes are small, no verbose.
+5. Start with a type of change (lowercase). (e.g., feat, fix, docs, style, refactor, test, chore).
 
-    const result = await model.generateContent(request);
-    const response = await result.response;
-    const text = response.text();
-    return text;
-  }
-  const commitMessages = await spinner(chalk.blue("generating..."), () =>
-    run(stagedChanges),
-  );
-  echo(chalk.blue("Success!\n"));
-  return commitMessages;
+Context (git diff --cached output): ${stagedChanges}`,
+                        },
+                    ],
+                },
+            ],
+        };
+
+        const result = await model.generateContent(request);
+        const response = await result.response;
+        const text = response.text();
+        return text;
+    }
+
+    const commitMessages = await spinner(chalk.blue("generating..."), () =>
+        run(stagedChanges),
+    );
+    echo(chalk.blue("Success!\n"));
+    return commitMessages;
 }
