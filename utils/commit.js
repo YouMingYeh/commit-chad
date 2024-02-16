6361584eb030c345acef989d4781a250ac092b81
YouMingYeh <b10705052@ntu.edu.tw>
2024-02-16 08:18:00 +0800

    chore: remove commented out code from runCommit function

--- utils/commit.js
+++ utils/commit.js
@@ -1,6 +1,4 @@
 export async function runCommit(commitMessages, config) {
-    // await $`npm version patch`;
-    // await $`git add package.json`;
   if (config.debug) {
     echo(chalk.blue("Commit logs: \n"));
     await $`git commit -m ${commitMessages}`;
